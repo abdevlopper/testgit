a1f1e0726863ff8c8724adf6f6ec5076e3478973
rabii <hichamaymane55@gmail.com>
2025-01-18 15:06:08 +0100

    ajout paragraphe+modifier chapitre 1

--- chap1.js
+++ chap1.js
@@ -1,8 +1,8 @@
-/*date = new Date ();
+date = new Date ();
 document.writeln ("nous somme ",date)
-function texte() { document.write("texte generee.");}*/
+function texte() { document.write("texte generee.");}
 
-/*var  x = 200
+var  x = 200
 document.writeln("x = ",x)
 if (x<20){
     document.write("nombre inferieur")
@@ -14,23 +14,23 @@ else
 var i;
 for ( i=0;i<10;i++){
     document.writeln(i);
-}*/
+}
 
 
-/*typeof { name:"hassan", age:34}*/
+typeof { name:"hassan", age:34}
 
-/* 1 boucle for
+// 1 boucle for
 const person = {fname:"Hassan", lname:"FILALI", age:25};
 
 let text = "";
 for (let x in person) { text += person[x];
 }
-document.write(text)*/  
-/* 2.array
+document.write(text)  
+// 2.array
 const numbers = [45, 4, 9, 16, 25]; 
 let txt = ""; numbers.forEach(myFunction);
 function myFunction(value, index, array) 
 { txt += value;
 }
-document.write(txt)*/
+document.write(txt)
 
