2c3e533cb95674ebce6c78596702f7741c3055e1
abdevlopper <abdomy41@gmail.com>
2025-01-18 14:35:38 +0100

    premiere commit

---
+++ chap1.js
@@ -0,0 +1,36 @@
+/*date = new Date ();
+document.writeln ("nous somme ",date)
+function texte() { document.write("texte generee.");}*/
+
+/*var  x = 200
+document.writeln("x = ",x)
+if (x<20){
+    document.write("nombre inferieur")
+}
+else
+{
+    document.write(" nombre superieur")
+}
+var i;
+for ( i=0;i<10;i++){
+    document.writeln(i);
+}*/
+
+
+/*typeof { name:"hassan", age:34}*/
+
+/* 1 boucle for
+const person = {fname:"Hassan", lname:"FILALI", age:25};
+
+let text = "";
+for (let x in person) { text += person[x];
+}
+document.write(text)*/  
+/* 2.array
+const numbers = [45, 4, 9, 16, 25]; 
+let txt = ""; numbers.forEach(myFunction);
+function myFunction(value, index, array) 
+{ txt += value;
+}
+document.write(txt)*/
+
